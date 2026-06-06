e3acd180f647e57c6af7df1c27bb1371774544f9
asrith-raju <asrithraju143@gmail.com>
2026-06-06 10:30:00 +0530

    client(feat): Enhance Features component with new layout and add Title component

---
+++ client/src/components/home/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = ({title,description}) => {
+  return (
+    <div className="text-center mt-6 text-slate-700">
+      
+    </div>
+  )
+}
+
+export default Title
