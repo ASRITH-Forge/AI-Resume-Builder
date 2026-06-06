badd2098f45df43a5dca3983d69819713e3c160d
asrith-raju <asrithraju143@gmail.com>
2026-06-06 10:30:00 +0530

    client(feat): Add Title component and integrate Testimonial section in Home page

---
+++ client/src/components/home/Testimonial.jsx
@@ -0,0 +1,17 @@
+import { BookUserIcon } from 'lucide-react'
+import Title from './Title'
+import React from 'react'
+
+const Testimonial = () => {
+  return (
+    <div id='testimonial' className="flex flex-col items-center my-10 scroll-mt-12">
+      <div className="flex items-center gap-2 text-sm text-green-600 bg-green-400/10  rounded-full px-4 py-1">
+                <BookUserIcon className='size-4.5 stroke-green-600'/>
+                <span>Testimonials</span>
+        </div>
+        <Title title="Don't just take our words" description="Hear what our users say about us. We're always looking for ways to improve. If you have a positive experience with us leave a review."/>
+    </div>
+  )
+}
+
+export default Testimonial
