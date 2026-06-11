0b962d2b9c25a80b6433633966e93397eef82122
asrith-raju <asrithraju143@gmail.com>
2026-06-11 10:30:00 +0530

    client(feat): Add container div for improved layout in Login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -19,6 +19,7 @@ const Login = () => {
         setFormData(prev => ({ ...prev, [name]: value }))
     }
   return (
+    <div className='flex items-center justify-center min-h-screen bg-gray-50'>
     <form onSubmit={handleSubmit} className="sm:w-[350px] w-full text-center border border-gray-300/60 rounded-2xl px-8 bg-white">
                 <h1 className="text-gray-900 text-3xl mt-10 font-medium">{state === "login" ? "Login" : "Sign up"}</h1>
                 <p className="text-gray-500 text-sm mt-2">Please sign in to continue</p>
@@ -43,7 +44,8 @@ const Login = () => {
                     {state === "login" ? "Login" : "Sign up"}
                 </button>
                 <p onClick={() => setState(prev => prev === "login" ? "register" : "login")} className="text-gray-500 text-sm mt-3 mb-11">{state === "login" ? "Don't have an account?" : "Already have an account?"} <a href="#" className="text-indigo-500 hover:underline">click here</a></p>
-            </form> 
+            </form>
+            </div> 
   )
 }
 
