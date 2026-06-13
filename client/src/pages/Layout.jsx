274f056122f94fa177621273206ba0281da698ad
asrith-raju <asrithraju143@gmail.com>
2026-06-13 10:30:00 +0530

    client(feat): Add Navbar component to the layout for improved navigation

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 const Layout = () => {
   return (
     <div>
-      <h1>Layout page</h1>
       <div>
         <Outlet/>
       </div>
