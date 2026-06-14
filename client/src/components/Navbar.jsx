3c680cc868b775df1fd4adbedc62ac63a937ebc2
asrith-raju <asrithraju143@gmail.com>
2026-06-14 10:30:00 +0530

    client(fix): Add logout functionality to Navbar button and improve Layout styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
          <Link><img src="/logo.svg" alt="Logo" className="h-11 w-auto"/></Link>
          <div className='flex items-center gap-4 text-sm'>
             <p className='max-sm:hidden'>Hi, {user?.name}</p>
-            <button onClick={} className='bg-white hover:bg-slate-50 border border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all'>Logout</button>
+            <button onClick={logoutUser} className='bg-white hover:bg-slate-50 border border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all'>Logout</button>
          </div>
       </nav>
     </div>
